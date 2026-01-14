c8b32777e17153b547dbf2c0f4f8b188b8d4bd8a
Kushagra Joshi <153926144+kushagraa27@users.noreply.github.com>
2026-01-14 23:04:41 +0530

    Create subArr_sumEQ_k.cpp

---
+++ Recursion & bactracking/subArr_sumEQ_k.cpp
@@ -0,0 +1,10 @@
+// problem: 560. Subarray Sum Equals K
+// difficulty: medium 
+// topic: hash table, array, prefix sum
+// approach: 
+// time:
+// space:
+// link: https://leetcode.com/problems/subarray-sum-equals-k/description/
+
+
+
