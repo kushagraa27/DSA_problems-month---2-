707552dffb289c92099fcda1d75be8c8cdf533af
Kushagra Joshi <153926144+kushagraa27@users.noreply.github.com>
2026-01-21 16:09:53 +0530

    Enhance comments with approach for middle index algorithm
    Added detailed approach explanation for the algorithm to find the middle index in an array.

--- prefix sum & subarray logic/findthe_middleIndex_inArr.cpp
+++ prefix sum & subarray logic/findthe_middleIndex_inArr.cpp
@@ -1,7 +1,10 @@
 // problem: 1991. Find the Middle Index in Array
 // difficulty: easy
 // topic: prefix sum, array
-// approach: 
+// approach: we take two variables, Lsum and Rsum to calculate left and right sum at each index or pivot, then we run loop one time to calculate total Rsum at starting 
+// then we again start a loop after computing Rsum, and i will become the pivot, Rsum will keep decreasing by Rsum-nums[i] and we will check if Rsum==Lsum, if yes then 
+// return i else simply update Lsum to Lsum+nums[i] and keep repeating this until u reach an end.
+// if loop ends then return -1.
 // time: O(n)
 // space: O(1)
 // link: https://leetcode.com/problems/find-the-middle-index-in-array/description/
